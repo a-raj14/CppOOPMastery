5eac7466690ab3538198328e2c691c54ab078c7a
Ayush Rajput <a.rajput6549@gmail.com>
2024-12-27 09:47:55 +0530

    Project structure modify and makefile setup

---
+++ MainApp.d
@@ -0,0 +1,4 @@
+obj/MainApp.o: D:\Projects\cplusplus\cplusplus/MainApp.cpp \
+ D:\Projects\cplusplus\cplusplus/OopsConcept/Abstraction.cpp \
+ D:\Projects\cplusplus\cplusplus/OopsConcept/Encapsulate.cpp \
+ D:\Projects\cplusplus\cplusplus/OopsConcept/Polymorphism.cpp
